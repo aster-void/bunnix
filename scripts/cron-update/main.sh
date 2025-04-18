29c3bc646298af7cf6c3dbaf96795c453f3e0dd2
aster <137767097+aster-void@users.noreply.github.com>
2025-04-19 00:02:00 +0900

    debug update action

--- scripts/cron-update/main.sh
+++ scripts/cron-update/main.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 #
 # ENV VARS
 # - AUTO_CHECKOUT?: "true" | "false" = false - automatically switches to new bump/bun-v(version) branch if update is found
-# - AUTO_COMMIT?: "true" | "false" = false - automatically commits if update is found
+# - AUTO_COMMIT?: "true" | "false" = false - automatically commits if update is found. only meant to be used in GitHub Actions.
 cd "$(dirname -- "$0")"
 
 NEW_VERSION=$(curl --silent https://api.github.com/repos/oven-sh/bun/releases/latest | jq '.tag_name | ltrimstr("bun-v")' --raw-output)
@@ -22,5 +22,5 @@ if "${AUTO_CHECKOUT:-false}"; then
 fi
 if "${AUTO_COMMIT:-false}"; then
   git add -A
-  git commit -m "chore: bump bun to v$NEW_VERSION"
+  git -c user.name="github-actions[bot]" -c user.email="41898282+github-actions[bot]@users.noreply.github.com" commit -m "chore: bump bun to v$NEW_VERSION"
 fi
