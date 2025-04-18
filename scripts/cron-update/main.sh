70c1f8bbfb967c7fc5147b1d68cdc10c8a5ee8f4
aster <137767097+aster-void@users.noreply.github.com>
2025-04-19 00:25:05 +0900

    use stdout to communicate

--- scripts/cron-update/main.sh
+++ scripts/cron-update/main.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-#
-# ENV VARS
-# - AUTO_CHECKOUT?: "true" | "false" = false - automatically switches to new bump/bun-v(version) branch if update is found
-# - AUTO_COMMIT?: "true" | "false" = false - automatically commits if update is found. only meant to be used in GitHub Actions.
 cd "$(dirname -- "$0")"
+
+printlog() {
+  echo -e "$@" >&2
+}
 
 NEW_VERSION=$(curl --silent https://api.github.com/repos/oven-sh/bun/releases/latest | jq '.tag_name | ltrimstr("bun-v")' --raw-output)
 
-echo "[update checker] latest bun = v$NEW_VERSION"
+printlog "[update checker] latest bun = v$NEW_VERSION"
 if grep --silent "^$NEW_VERSION$" ../../supported_versions ; then
-  echo '[update checker] supported_version is up to date'
+  printlog '[update checker] supported_version is up to date'
   exit 0
 fi
 
-../fetch-hash/main.sh "$NEW_VERSION" 2>&1
+../fetch-hash/main.sh "$NEW_VERSION"
 
-if "${AUTO_CHECKOUT:-false}"; then
-  git switch --create "bump/bun-v$NEW_VERSION"
-fi
-if "${AUTO_COMMIT:-false}"; then
-  git add -A
-  git -c user.name="github-actions[bot]" -c user.email="41898282+github-actions[bot]@users.noreply.github.com" commit -m "chore: bump bun to v$NEW_VERSION"
-fi
+printlog -n 'New version is: '
+echo "$NEW_VERSION"
