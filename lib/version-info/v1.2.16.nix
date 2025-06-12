d8bb6c038398ff4b4113446b392976c7e9498eab
aster-void <137767097+aster-void@users.noreply.github.com>
2025-06-12 06:15:17 +0000

    bump: bun to v1.2.16

---
+++ lib/version-info/v1.2.16.nix
@@ -0,0 +1,9 @@
+{
+  hashes = {
+    aarch64-darwin = "sha256-IBaCFzMLDruOkUg25jqvVxQK/wz5VxYidlh8qwQS8X8=";
+    aarch64-linux = "sha256-gKAkQ0q1sJjCWe5VsfWCgFkdHeKGxcZStjNkCoyZ5j4=";
+    x86_64-darwin = "sha256-WPEJ8m81EF1RogdpAOLhdQamBY7lk6NMRZ3IeeUiHnQ=";
+    x86_64-linux = "sha256-+DEdjXyqDZOMbEzs8KYA5enOTidaQR44oun4x30MEAI=";
+  };
+  builder-version = "v1";
+}
