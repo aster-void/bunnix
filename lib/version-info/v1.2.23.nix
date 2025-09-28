0952b696042635644a18549acfc09715936bcd54
aster-void <137767097+aster-void@users.noreply.github.com>
2025-09-28 15:08:06 +0000

    bump: bun to v1.2.23

---
+++ lib/version-info/v1.2.23.nix
@@ -0,0 +1,9 @@
+{
+  hashes = {
+    aarch64-darwin = "sha256-IvX6P/9ysNO45+D4BR7K3y5Bkg1HSsYttSeRRICckAU=";
+    aarch64-linux = "sha256-anqYxUbQhKhF3tpi6ypblKahSmPqgc+RhtRr9Vv5EKk=";
+    x86_64-darwin = "sha256-/MZyPIq6ZGpYRDWG0XhrkJ5kBh5YS0IUoLZ+BFElKEU=";
+    x86_64-linux = "sha256-zw7QqSB5nVdv/eTgyuZtcyvyPCUwQH8m9Zx4Md/+Hw4=";
+  };
+  builder-version = "v1";
+}
